0047843ba71bf1f56765b0542a5bce2af62c8000
JoseLuisDavid19 <Joseluisyoda@gmail.com>
2019-03-08 19:03:48 -0600

    lab5

---
+++ src/PokemonList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import LoadingPokemon from './LoadingPokemon';
+
+class PokemonList extends Component {
+    render() {
+      return (
+        <div>
+         <LoadingPokemon/>
+        </div>
+      );
+    }
+  }
+  
+  export default PokemonList;
+  
